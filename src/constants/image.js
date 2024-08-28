005102c9d568fc2c5727728510738bccab6e0dfd
Ashutosh Verma <ashutoshrgnict@gmail.com>
2024-08-29 00:38:54 +0530

    chore: Update project dependencies and configuration files

---
+++ src/constants/image.js
@@ -0,0 +1,45 @@
+import upworkIcon from "../assets/upwork.svg";
+import emailIcon from "../assets/email.svg";
+import githubIcon from "../assets/github.svg";
+import linkedinIcon from "../assets/linkedin.svg";
+import htmlLogo from "../assets/logo/html-logo.svg";
+import cssLogo from "../assets/logo/css-logo.svg";
+import javascriptLogo from "../assets/logo/javascript-logo.svg";
+import reactLogo from "../assets/logo/react-logo.svg";
+import tailwindLogo from "../assets/logo/tailwind-logo.svg";
+import gitLogo from "../assets/logo/git-logo.svg";
+import githubLogo from "../assets/logo/github-logo.svg";
+import nextLogo from "../assets/logo/next-logo.svg";
+import photography from "../assets/projects/photography-website.png";
+import errorIllustration from "../assets/illustrations/404-vector.svg";
+import successIllustration from "../assets/illustrations/success-vector.svg";
+import brandLogoLight from "../assets/icons/logo-light.png";
+import brandLogoDark from "../assets/icons/logo-dark.png";
+import hindlionss from "../assets/projects/hindlionss.png";
+import rickshaw from "../assets/projects/rickshaw.png";
+import saathi from "../assets/projects/saathi-website.png";
+import myProfile from "../assets/me.jpg";
+
+export default {
+  emailIcon,
+  upworkIcon,
+  githubIcon,
+  linkedinIcon,
+  htmlLogo,
+  cssLogo,
+  javascriptLogo,
+  reactLogo,
+  tailwindLogo,
+  gitLogo,
+  githubLogo,
+  nextLogo,
+  photography,
+  errorIllustration,
+  successIllustration,
+  brandLogoLight,
+  brandLogoDark,
+  hindlionss,
+  rickshaw,
+  saathi,
+  myProfile,
+};
